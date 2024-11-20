dfbe673340bce70330a7be8505b9f92923954e74
DefinitelyNotMigZ <103237073+DefinitelyNotMigZ@users.noreply.github.com>
2024-11-20 22:29:06 +0800

    SIYA PA RIN

--- client/src/config/supabaseClient.js
+++ client/src/config/supabaseClient.js
@@ -4,7 +4,7 @@ console.log('SUPABASE URL:', process.env.REACT_APP_SUPABASE_URL);
 console.log('SUPABASE KEY:', process.env.REACT_APP_SUPABASE_ANON_KEY);
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || "https://uavbcuwklwnseoepnihz.supabase.co";
-const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhdmJjdXdrbHduc2VvZXBuaWh6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE4NzMxOTEsImV4cCI6MjA0NzQ0OTE5MX0.4C7FbRGxMI6SkQ6FKh_nNJWMJ26XUGoSbTJrnfkWV1Y";
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhdmJjdXdrbHduc2VvZXBuaWh6Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMTg3MzE5MSwiZXhwIjoyMDQ3NDQ5MTkxfQ.F5dNI4_YoU1UkO5uHLygFbNz8WvBk5hajJmsDUSb-h0";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
